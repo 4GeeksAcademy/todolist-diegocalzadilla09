aaa7859283371019135c47bd5baf04c395ad2802
diegocalzadilla09 <diegocalzadilla09@gmail.com>
2026-01-07 00:44:53 +0000

    version 1

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
+import "../../styles/index.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+ //create your first component
+ const Home = () => {
+
+ const [task, setTask]=useState("")
+ const [taskList, setTaskList]=useState(["Wash dishes", "Wash the car","Cook","Clean the bathroom"])
+ function addTask(){
+	setTaskList([...taskList,task])
+}
+	
+	
+	
 	return (
-		<div className="text-center">
+		<div className="text-center d-flex flex-column">
+
+			<div className="todo-box mx-auto mt-5">
             
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+			<h1 className="text-center mb-3">To Do</h1>
+			<input type="text" 
+				className="form-control"
+				placeholder="What needs to be done?" 
+				value={task}	
+				onChange={event=>setTask(event.target.value)}
+				onKeyUp={(e) => {
+					if (e.key === "Enter") {
+						setTaskList([...taskList, task]);
+						setTask("");
+					}
+				}}
+			/>
+			<ul className="list-unstyled mb-0">
+				{taskList.length === 0 ? (
+				<li className= "empty-msg">You haven't added any task yet.</li>
+				) : (
+				taskList.map((task, index) => (
+					<li className="todo-item" key={index}>
+						<span>{task}</span>
+
+						<span
+							className="delete-btn"
+							onClick={() =>
+								setTaskList(taskList.filter((item, ind) => ind !== index))
+							}
+							>
+								x
+							</span>
+						</li>
+				))
+			)}
+
+			</ul>
+			<p className="count">
+				{taskList.length} item{taskList.lenght !== 1 ? "s" : ""} left
 			</p>
 		</div>
+	</div>
 	);
 };
 
